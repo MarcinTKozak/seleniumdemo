51deca059f51ebdfeab635f0f84dc2ff228db82f
Marcin <marcin.t.kozak@gmail.com>
2022-07-04 22:13:17 +0100

    add tests and page objects

--- src/test/java/pl/testeroprogramowania/tests/BaseTest.java
+++ src/test/java/pl/testeroprogramowania/tests/BaseTest.java
@@ -3,7 +3,6 @@ package pl.testeroprogramowania.tests;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import pl.testeroprogramowania.utils.DriverFactory;
 
 import java.util.concurrent.TimeUnit;
